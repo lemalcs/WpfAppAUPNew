71fb7f50e31b4ba562fe16964a5fa012a5e3b90c
Luis <info@lemaluis.com>
2021-11-03 23:08:19 -0500

    Initial emition.

---
+++ WpfAppAUPNew/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using AutoUpdaterDotNET;
+
+namespace WpfAppAUP
+{
+    /// <summary>
+    /// Main Window
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You clicked the button", "AUtoUpdate", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            AutoUpdater.RunUpdateAsAdmin = false;
+            AutoUpdater.Start(@"AppAUPUpdater.xml");
+        }
+    }
+}
